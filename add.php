116da9e24bdb4e5390cd8e0cc2725be5cd81c848
Charlotte GAERTNER <charlotte.gaertner@outlook.fr>
2023-11-06 22:29:35 +0100

    add option where existing author check

--- add.php
+++ add.php
@@ -5,74 +5,115 @@ require '_connec.php';
 $pdo = new \PDO(DSN, USER, PASS);
 
 if (!empty($_POST)) {
-    var_dump($_POST);
-    $title = $_POST['title'];
-    $lastname = $_POST['lastname'];
-    $firstname = $_POST['firstname'];
-    $price = $_POST['price'];
-    $resume = $_POST['resume'];
-    $categories = isset($_POST["category"]) ?
-    $_POST["category"]:[];
-    $date = $_POST['date'];
-
-
-    
-    try {  
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->beginTransaction();
-        $query = 'INSERT INTO book (title, `resume`, `date`, price) VALUES (:title, :resume, :date, :price)';
-        $statement =  $pdo ->prepare($query);
-        $statement ->bindValue(':title', $title, \PDO::PARAM_STR);
-        $statement ->bindValue(':resume', $resume, \PDO::PARAM_STR);
-        $statement ->bindValue(':date', $date, PDO::PARAM_STR);
-        $statement ->bindValue(':price', $price, PDO::PARAM_STR);
-        $statement ->execute();
-        $idbook = $pdo->lastInsertId();
-        $query2 = "INSERT INTO author (lastname, firstname) VALUES (:lastname, :firstname)";
-        $statement2 = $pdo ->prepare($query2);
-        $statement2 ->bindValue(':lastname', $lastname, \PDO::PARAM_STR);
-        $statement2 ->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
-        $statement2 ->execute();
-        $idauthor = $pdo->lastInsertId();
-        foreach ($categories as $category) {
-            $query3 = "INSERT INTO book_category (idbook, idcategory) VALUES (:idbook, :idcategory)";
-            $statement3 = $pdo ->prepare($query3);
-            $statement3 ->bindValue(':idbook', $idbook, \PDO::PARAM_INT);
-            $statement3 ->bindValue(':idcategory', $category, \PDO::PARAM_STR);
-            $statement3 -> execute();
+    $error = 0;
+    if ((empty($_POST['title'])) || (mb_strlen($_POST['title']) > 45)) {
+        echo "Vous devez entrer un nom de livre de moins de 45 caractères.<br/>";
+        $error ++;
+    }; 
+    if ((empty($_POST['lastname'])) || (mb_strlen($_POST['lastname']) > 45)) {
+        echo "Vous devez entrer un nom d'auteur de moins de 45 caractères.<br/>";
+        $error ++;
+    };
+    if ((empty($_POST['firstname'])) || (mb_strlen($_POST['firstname']) > 45)) {
+        echo "Vous devez entrer un prénom d'auteur de moins de 45 caractères.<br/>";
+        $error ++;
+    };
+    if (empty($_POST['category'])) {
+        echo "Vous devez entrer au moins un genre.<br/>";
+        $error ++;
+    };
+    if (isset($_POST['resume'])) {
+        if (mb_strlen($_POST['resume']) > 1500) {
+        echo "Votre résumé doit faire moins de 1500 caractères.<br/>";
+        $error ++;
         };
-        $query4 ="INSERT INTO book_author (idbook, idauthor) VALUES (:idbook, :idauthor)";
-        $statement4 = $pdo ->prepare($query4);
-        $statement4 ->bindValue(':idbook', $idbook, \PDO::PARAM_INT);
-        $statement4 ->bindValue(':idauthor', $idauthor, \PDO::PARAM_INT);
-        $statement4 -> execute();
-        $pdo->commit();
+    };
+    if ($_POST['price'] < 0) {
+        echo "Le prix doit être positif.<br/>";
+        $error ++;
+    };
+    $currentDate = date('Y-m-d');
+    if (empty($_POST['date']) || $_POST['date'] > $currentDate) {
+        echo "La date n'est pas valide.<br/>";
+        $error++;
+    };
+    if ($error === 0) {
+        $title = trim($_POST['title']);
+        $lastname = trim(ucfirst(strtolower($_POST['lastname'])));
+        $firstname = trim(ucfirst(strtolower($_POST['firstname'])));
+        $price = $_POST['price'];
+        $resume = $_POST['resume'];
+        $categories = isset($_POST["category"]) ?
+        $_POST["category"]:[];
+        $date = $_POST['date'];
+
+        try {  
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->beginTransaction();
+            $query = 'INSERT INTO book (title, `resume`, `date`, price) VALUES (:title, :resume, :date, :price)';
+            $statement =  $pdo ->prepare($query);
+            $statement ->bindValue(':title', $title, \PDO::PARAM_STR);
+            $statement ->bindValue(':resume', $resume, \PDO::PARAM_STR);
+            $statement ->bindValue(':date', $date, PDO::PARAM_STR);
+            $statement ->bindValue(':price', $price, PDO::PARAM_STR);
+            $statement ->execute();
+            $idbook = $pdo->lastInsertId();
+            $queryExistingAuthor = "SELECT idauthor FROM author WHERE lastname = :lastname AND firstname = :firstname";
+            //
+            $statementExistingAuthor = $pdo->prepare($queryExistingAuthor);
+            $statementExistingAuthor->bindValue(':lastname', $lastname, PDO::PARAM_STR);
+            $statementExistingAuthor->bindValue(':firstname', $firstname, PDO::PARAM_STR);
+            $statementExistingAuthor->execute();
+            $existingAuthor = $statementExistingAuthor->fetch();
+            if (!empty($existingAuthor)) {
+                $idauthor = $existingAuthor['idauthor'];
+            } else {
+                $query2 = "INSERT INTO author (lastname, firstname) VALUES (:lastname, :firstname)";
+                $statement2 = $pdo ->prepare($query2);
+                $statement2 ->bindValue(':lastname', $lastname, \PDO::PARAM_STR);
+                $statement2 ->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
+                $statement2 ->execute();
+                $idauthor = $pdo->lastInsertId();
+            };
+            foreach ($categories as $category) {
+                $query3 = "INSERT INTO book_category (idbook, idcategory) VALUES (:idbook, :idcategory)";
+                $statement3 = $pdo ->prepare($query3);
+                $statement3 ->bindValue(':idbook', $idbook, \PDO::PARAM_INT);
+                $statement3 ->bindValue(':idcategory', $category, \PDO::PARAM_INT);
+                $statement3 -> execute();
+            };
+            $query4 ="INSERT INTO book_author (idbook, idauthor) VALUES (:idbook, :idauthor)";
+            $statement4 = $pdo ->prepare($query4);
+            $statement4 ->bindValue(':idbook', $idbook, \PDO::PARAM_INT);
+            $statement4 ->bindValue(':idauthor', $idauthor, \PDO::PARAM_INT);
+            $statement4 -> execute();
+            $pdo->commit();
+            
         
-      
-        } catch (Exception $e) {
-        $pdo->rollBack();
-        echo "Failed: " . $e->getMessage();
-        };
-header('Location: /index.php');
-die;
+            } catch (Exception $e) {
+                $pdo->rollBack();
+                echo "Failed: " . $e->getMessage();
+            };
+    header('Location: /index.php');
+    die;
+    };
 };
-
 echo "Ajouter un livre à la bibliothèque.<br/>";
 echo '<form method="post">';
 echo '<table>';
-echo '<tr><th>Titre</th><td><input type="text" maxlength="45" name="title" value=""/></td></tr>';
-echo '<tr><th>Nom de l\'auteur</th><td><input type="text" maxlength="45" name="lastname" value=""/></td></tr>';
-echo '<tr><th>Prénom de l\'auteur</th><td><input type="text" maxlength="45"  name="firstname" value=""/></td></tr>';
+echo '<tr><th>Titre*</th><td><input type="text" maxlength="45" name="title" value="" required/></td></tr>';
+echo '<tr><th>Nom de l\'auteur*</th><td><input type="text" maxlength="45" name="lastname" value="" required/></td></tr>';
+echo '<tr><th>Prénom de l\'auteur*</th><td><input type="text" maxlength="45"  name="firstname" value=""required/></td></tr>';
 echo '<tr><th>Résumé</th><td><textarea name="resume" maxlength="1500" rows="5" cols="15" value=""></textarea></td></tr>';
 $query2 = "SELECT * FROM `category`";
 $statement1 = $pdo->query($query2);
 $categories = $statement1->fetchAll(PDO::FETCH_ASSOC);
-echo '<tr>';
+echo '<tr><td>Il faut choisir au moins 1 genre.</td><td></td>';
 foreach ($categories as $category) {
     echo '<td><label for="category[]">' . $category['category'] . '</label><input name="category[]" type="checkbox" id="' . $category['idcategory'] . '"value="' . $category['idcategory'] . '"/></td>';
 };
 echo '</tr>';
-echo '<tr><th>Date d\'édition</th><td><input type="date" name="date" value=""/></td></tr>';
+echo '<tr><th>Date d\'édition*</th><td><input type="date" name="date" value=""/></td></tr>';
 echo '<tr><th>Prix</th><td><input type="number" step="0.01" name="price" value=""/></td></tr>';
 echo '<tr><td><input type="submit" value="Enregistrer"></td></tr>';
 echo '</table>';
--- edit.php
+++ edit.php
@@ -15,26 +15,58 @@ if (!empty($_POST)) {
     $firstname = $_POST['firstname'];
     $firstname = $_POST['firstname'];
     $resume = $_POST['resume'];
+    $price = $_POST['price'];
     $categories = isset($_POST["category"]) 
         ? $_POST["category"]
         :[];
     $date = $_POST['date'];
+    $idbook = $_GET['identifiant'];
 
-    $query = "UPDATE book SET title = :title, `resume` = :resume WHERE idbook = :identifiant";
+    
+    try {  
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->beginTransaction();
+    $query = "UPDATE book SET title = :title, `resume` = :resume, `date` = :date, price = :price WHERE idbook = :identifiant";
     $statement = $pdo->prepare($query);
     $statement ->bindValue(':title', $title, \PDO::PARAM_STR);
     $statement ->bindValue(':resume', $resume, \PDO::PARAM_STR);
-    $statement ->bindValue(':identifiant', $_GET['identifiant'], \PDO::PARAM_INT);
+    $statement ->bindValue(':date', $date, PDO::PARAM_STR);
+    $statement ->bindValue(':price', $price, PDO::PARAM_STR);
+    $statement ->bindValue(':identifiant', $idbook, \PDO::PARAM_INT);
     $statement ->execute();
+    $query2 = "UPDATE author SET lastname = :lastname, firstname = :firstname";
+    $statement2 = $pdo ->prepare($query2);
+    $statement2 ->bindValue(':lastname', $lastname, \PDO::PARAM_STR);
+    $statement2 ->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
+    $statement2 ->execute();
+    $idauthor = $pdo->lastInsertId();
+    foreach ($categories as $category) {
+        $query3 = "UPDATE book_category SET idbook = :idbook, idcategory = :idcategory";
+        $statement3 = $pdo ->prepare($query3);
+        $statement3 ->bindValue(':idbook', $idbook, \PDO::PARAM_INT);
+        $statement3 ->bindValue(':idcategory', $category, \PDO::PARAM_INT);
+        $statement3 -> execute();
+    };
+    $query4 ="UPDATE book_author SET idbook = :idbook, idauthor = :idauthor";
+    $statement4 = $pdo ->prepare($query4);
+    $statement4 ->bindValue(':idbook', $idbook, \PDO::PARAM_INT);
+    $statement4 ->bindValue(':idauthor', $idauthor, \PDO::PARAM_INT);
+    $statement4 -> execute();
+    $pdo->commit();
+        
+    } catch (Exception $e) {
+        $pdo->rollBack();
+        echo "Failed: " . $e->getMessage();
+    };
 };
 
 // affiche les infos du livre
-$query2 = "SELECT title, lastname, firstname, price, `resume`, GROUP_CONCAT(`category` SEPARATOR ', ') as genre, `date` FROM `book` INNER JOIN book_author ON book.idbook=book_author.idbook
+$query5 = "SELECT title, lastname, firstname, price, `resume`, GROUP_CONCAT(`category` SEPARATOR ', ') as genre, `date` FROM `book` INNER JOIN book_author ON book.idbook=book_author.idbook
 INNER JOIN author ON author.idauthor = book_author.idauthor
 INNER JOIN book_category ON book_category.idbook = book.idbook
 INNER JOIN category ON category.idcategory = book_category.idcategory WHERE book.idbook = " . $_GET['identifiant'] . " GROUP BY title, lastname, firstname, price , `resume`, `date` ";
-$statement2 = $pdo->query($query2);
-$book = $statement2->fetchAll(PDO::FETCH_ASSOC);
+$statement5 = $pdo->query($query5);
+$book = $statement5->fetchAll(PDO::FETCH_ASSOC);
 
 echo '<h2>Modifier les informations du livre "' . $book[0]['title'] . '".</h2><br/<br/>';
 
@@ -46,15 +78,16 @@ echo '<tr><th>Titre</th><td><input typr="text" name="title" value="' . $book[0][
 echo '<tr><th>Nom de l\'auteur</th><td><input type="text" name="lastname" value="' . $book[0]['lastname'] . '"/></td></tr>';
 echo '<tr><th>Prénom de l\'auteur</th><td><input type="text" name="firstname" value="' . $book[0]['firstname'] . '"/></td></tr>';
 echo '<tr><th>Résumé</th><td><textarea rows="6" cols="40" id="resume" name="resume" >' . $book[0]['resume'] . '</textarea></td></tr>';
-$query3 = "SELECT * FROM `category`";
-$statement3 = $pdo->query($query3);
-$categories = $statement3->fetchAll(PDO::FETCH_ASSOC);
+$query6 = "SELECT * FROM `category`";
+$statement6 = $pdo->query($query6);
+$categories = $statement6->fetchAll(PDO::FETCH_ASSOC);
 echo '<tr>';
 foreach ($categories as $category) {
     echo '<td><label for="categoty[]">' . $category['category'] . '</label><input name="category[]" type="checkbox" id="' . $category['idcategory'] . '"value="' . $category['idcategory'] . '" /></td>';
 };
 echo '</tr>';
 echo '<tr><th>Date d\'édition</th><td><input type="date" name="date" value="' . $book[0]['date'] . '"/></td></tr>';
+echo '<tr><th>Prix</th><td><input type="number" step="0.01" name="price" value="' . $book[0]['price'] . '"/></td></tr>';
 echo '<tr><td><input type="submit" value="Enregistrer"></td></tr>';
 echo '</table>';
 echo '</form>';
